eb6a9fb102a7b29230834cde45057a5f2468fb95
Your Name <you@example.com>
2015-09-09 08:11:45 +0300

    add sass sample add scss sample

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,4 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( page1.css )
+Rails.application.config.assets.precompile += %w( page1.css page2.css page3.css )
